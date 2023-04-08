69cfe82f1d1403eb3a592f6a01aee7f2df50b61f
Oscar Boccuti <oscarboccuti@gmail.com>
2023-04-08 19:42:55 +0200

    primer commit

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ if (localStorage.getItem("ratesData")) {
 // Display currency rates in dropdowns
 function displayRates(data) {
   const entries = Object.entries(data);
-  for (var i = 0; i < entries.length; i++) {
+  for (let i = 0; i < entries.length; i++) {
     select[0].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
     select[1].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
   }
